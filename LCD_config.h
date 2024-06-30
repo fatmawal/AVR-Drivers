fda7f0ff1b8d3050279467f2516f256aaeae7eeb
fatma waleed <163358279+fatmawal@users.noreply.github.com>
2024-07-01 00:23:57 +0300

    Create LCD_config.h

---
+++ LCD_config.h
@@ -0,0 +1,31 @@
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+
+/*Options:
+ * PORTA_REG
+ * PORTB_REG
+ * PORTC_REG
+ * PORTD_REG
+ */
+
+/*Options:
+ * PIN0
+ * PIN1
+ * PIN2
+ * PIN3
+ * PIN4
+ * PIN5
+ * PIN6
+ * PIN7
+ */
+
+#define DATA_PORT     PORTA_REG
+
+#define CTRL_PORT     PORTC_REG
+#define RS     PIN0
+#define RW     PIN1
+#define E      PIN2
+
+#endif
